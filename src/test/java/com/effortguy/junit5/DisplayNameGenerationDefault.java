7e26baa3d53f1759eaa6987d232453a902201c25
hyjung <hyjung@tmon.co.kr>
2020-12-30 23:43:39 +0900

    DisplayName Default 설정

---
+++ src/test/java/com/effortguy/junit5/DisplayNameGenerationDefault.java
@@ -0,0 +1,10 @@
+package com.effortguy.junit5;
+
+import org.junit.jupiter.api.Test;
+
+public class DisplayNameGenerationDefault {
+
+    @Test
+    void test_display_name_generator_default() {
+    }
+}
